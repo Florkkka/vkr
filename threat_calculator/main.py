149d57afda094aa7dba524cf8383b5ad4a712fe1
Florkkka <134052572+Florkkka@users.noreply.github.com>
2026-06-20 21:09:46 +0300

    Update main.py

--- threat_calculator/main.py
+++ threat_calculator/main.py
@@ -657,4 +657,12 @@ if __name__ == "__main__":
     import os
     
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    
+    reload = os.environ.get("ENV", "production") == "development"
+    
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=reload
+    )
